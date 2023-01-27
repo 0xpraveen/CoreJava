dc925f352e601620e71d45b45a43515d65109e24
Praveen Chaurasia <108619467+0xpraveen@users.noreply.github.com>
2023-01-27 09:54:50 +0530

    Create 2701_Assignment.java

---
+++ Assignment/2701_Assignment.java
@@ -0,0 +1,5 @@
+//Question
+//1. Find if the linked list is palindrome without using extra space.
+//Example: 1->2->3->2->: 0/p:Yes
+//2. Given a integer arrayList, rotate it by d elements to its left.
+//Example: [1 2 3 4 5], d= 3 o/p : [4 5 1 2 3]
