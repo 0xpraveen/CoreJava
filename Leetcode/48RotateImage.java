c412ba3cac43a5341005188f6bd24dabf82fd2c7
Praveen Chaurasia <108619467+0xpraveen@users.noreply.github.com>
2023-02-08 19:24:30 +0530

    Update 48RotateImage.java

--- Leetcode/48RotateImage.java
+++ Leetcode/48RotateImage.java
@@ -1,32 +1,24 @@
 class Solution {
     public void rotate(int[][] matrix) {
-      for (int i = 0; i < matrix.length; i++) {
-            for (int j = i; j < matrix[0].length; j++) {
-                int temp = 0;
-                temp = matrix[i][j];
+  int rows = matrix.length;
+        int columns = matrix[0].length;
+    
+    
+        for(int i=0; i<rows; i++){
+            for(int j=i; j<columns; j++){
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length / 2; j++) {
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length - 1 - j];
-                matrix[i][matrix.length - 1 - j] = temp;
+        
+    
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][columns-1-j];
+                matrix[i][columns-1-j] = temp;
             }
         }
-    }
-
-    public  void main(String args[]) {
-        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        rotate(arr);
-        System.out.println("Rotated Image");
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();  
-    }
     }
 }
